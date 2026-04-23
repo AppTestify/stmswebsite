52602d44fd3e4767791b1cc412e7f6e5df386531
RahulK7070 <rahul.c@apptestify.com>
2026-04-23 16:47:10 +0530

    Fixed the Alignment of CollegeAnthem Page

--- src/pages/CollegeAnthem.jsx
+++ src/pages/CollegeAnthem.jsx
@@ -35,7 +35,7 @@ const CollegeAnthem = () => {
 
       {/* Images Section */}
       <section className="py-16 bg-white">
-        <div className="max-w-6xl ">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div className="bg-white rounded-xl shadow-lg overflow-hidden">
               <img 
